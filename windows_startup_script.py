e5f8df48a08fb42713d4403ddeca1ed4c0d6f5d4
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-25 14:04:16 +0700

    Create windows_startup_script.py

---
+++ windows_startup_script.py
@@ -0,0 +1,6 @@
+from telethon import TelegramClient
+API_KEY="Ketik ini di sini"
+API_HASH="Ketik ini di sini"
+#my.telegram.org adresinden alın
+bot = TelegramClient('userbot',API_KEY,API_HASH)
+bot.start()
