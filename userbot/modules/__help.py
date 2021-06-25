75c2ba879d492feecba7ec004a767c30c0decd72
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-26 06:26:32 +0700

    Create __help.py

---
+++ userbot/modules/__help.py
@@ -0,0 +1,36 @@
+
+from userbot.cmdhelp import CmdHelp
+from userbot import cmdhelp
+from userbot import CMD_HELP
+from userbot.events import register
+
+# ██████ LANGUAGE CONSTANTS ██████ #
+
+from userbot.language import get_value
+LANG = get_value("alpha")
+
+# ████████████████████████████████ #
+
+@register(outgoing=True, pattern="^.help(?: |$)(.*)")
+async def alpha(event):
+    """ modules Alpha Userbot """
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            await event.edit(str(CMD_HELP[args]))
+        else:
+            await event.edit(LANG["NEED_PLUGIN"])
+    else:
+        string = ""
+        sayfa = [sorted(list(CMD_HELP))[i:i + 5] for i in range(0, len(sorted(list(CMD_HELP))), 5)]
+        
+        for i in sayfa:
+            string += f'`⚡ `'
+            for sira, a in enumerate(i):
+                string += "`" + str(a)
+                if sira == i.index(i[-1]):
+                    string += "`"
+                else:
+                    string += "`, "
+            string += "\n"
+        await event.edit(LANG["NEED_MODULE"] + '\n\n' + string)
